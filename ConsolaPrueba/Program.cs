ba8c04c91ece99c986d319c75f671dd70c19bcb4
Enrique <enrique.reyesmarin@gmail.com>
2020-11-10 10:43:09 -0500

    Agregar archivos de proyecto.

---
+++ ConsolaPrueba/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la PUCESE");
+            Console.WriteLine("Segundo de Tic");
+            //Fin de Clase
+        }
+    }
+}
